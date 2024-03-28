1fe21cc2b917ef3c1215f52ed5dc48406ad63802
Tarun sharma <tarunsharma.9097@gmail.com>
2024-03-28 16:34:13 +0530

    Add project files.

---
+++ Assignment4_BlogApp/Data/BlogAppContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment4_BlogApp.Models;
+
+    public class BlogAppContext : DbContext
+    {
+        public BlogAppContext (DbContextOptions<BlogAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment4_BlogApp.Models.UserData> UserData { get; set; } = default!;
+    }
---
+++ Assignment4_BlogApp/Models/UserData.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment4_BlogApp.Models
+{
+    public class UserData
+    {
+        [Key]
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string Content { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
